b0b129649048f4924098075abf7cf74303cec92f
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-12-22 07:51:48 +0300

    Move RPCResponse decode to separate file

---
+++ Sources/XMLRPC/RPCResponse+Decode.swift
@@ -0,0 +1,28 @@
+import XML
+import Stream
+
+extension RPCResponse {
+    public init<T: UnsafeStreamReader>(from stream: T) throws {
+        let document = try XML.Document(from: stream)
+        guard let root = document.root, root.name == "methodResponse" else {
+            throw RPCError.invalidXML
+        }
+
+        var params = [RPCValue]()
+
+        for param in root["params"].children {
+            guard let value = param["value"].children.first else {
+                throw RPCValueError(
+                    reason: .empty,
+                    description: "xml element has no content")
+            }
+            guard let element = XML.Element(value) else {
+                throw RPCValueError(
+                    reason: .invalidValue,
+                    description: "xml element was expected")
+            }
+            params.append(try RPCValue(from: element))
+        }
+        self.params = params
+    }
+}
--- Sources/XMLRPC/RPCResponse.swift
+++ Sources/XMLRPC/RPCResponse.swift
@@ -1,36 +1,9 @@
 import XML
-import Stream
 
 public struct RPCResponse {
     public let params: [RPCValue]
 
     public init(params: [RPCValue]) {
-        self.params = params
-    }
-}
-
-extension RPCResponse {
-    public init<T: UnsafeStreamReader>(from stream: T) throws {
-        let document = try XML.Document(from: stream)
-        guard let root = document.root, root.name == "methodResponse" else {
-            throw RPCError.invalidXML
-        }
-
-        var params = [RPCValue]()
-
-        for param in root["params"].children {
-            guard let value = param["value"].children.first else {
-                throw RPCValueError(
-                    reason: .empty,
-                    description: "xml element has no content")
-            }
-            guard let element = XML.Element(value) else {
-                throw RPCValueError(
-                    reason: .invalidValue,
-                    description: "xml element was expected")
-            }
-            params.append(try RPCValue(from: element))
-        }
         self.params = params
     }
 }
